bdac6670f19579d95b7dd5806560848d6e778e1d
Abhinav Goel <amgoel@mit.edu>
2023-11-19 16:49:18 -0500

    anypixel

--- hdl/map_sprite_1.sv
+++ hdl/map_sprite_1.sv
@@ -13,8 +13,8 @@ module map_sprite_1 #(
   input wire rst_in,
   input wire [10:0] hcount_in,
   input wire [9:0]  vcount_in,
-  input wire [7:0] ballx,
-  input wire [6:0] bally,
+  input wire [15:0] ballx,
+  input wire [15:0] bally,
   input wire [15:0] angle,
   output logic [7:0] red_out,
   output logic [7:0] green_out,
@@ -48,21 +48,26 @@ module map_sprite_1 #(
   
 
   // calculate rom address
+  logic [10:0] ballposx;
+  logic [9:0] ballposy;
   logic [$clog2(WIDTH*HEIGHT)-1:0] image_addr;
-  logic [5:0] circle_addr;
   logic [7:0] pixelx;
   logic [6:0] pixely;
   logic [2:0] lox;
   logic [2:0] loy;
+  assign ballposx = ballx[15:5];
+  assign ballposy = bally[14:5];
   assign pixelx = hcount_in/8;
+  assign balllox = (ballx-hcount_in+3)%8;
+  assign ballloy = (bally-vcount_in+3)%8;
   assign lox = hcount_in % 8;
   assign pixely = vcount_in/8;
   assign loy = vcount_in % 8;
   assign image_addr = pixelx + (pixely * WIDTH);
   logic isball;
-  assign isball = (ballx == pixelx) & (bally == pixely);
-  assign circle_addr = lox + loy*8;
+  assign isball = (ballposx-3 <= hcount_in & ballposx+4 >= hcount_in) & (ballposy-3 <= vcount_in & ballposy+4 >= vcount_in);
   logic circle_out;
+  logic ball_circle_out;
 
 
   
@@ -75,6 +80,7 @@ module map_sprite_1 #(
   logic [23:0] finalcolors;
   logic imagesprite_pipe [3:0];
   logic circleout_pipe [3:0];
+  logic ballout_pipe [3:0];
   always_ff @(posedge pixel_clk_in)begin
     if(lox == 0 | lox == 7) begin
       if(loy == 0 | loy == 1 | loy == 6 | loy == 7 ) begin
@@ -91,16 +97,34 @@ module map_sprite_1 #(
     end else begin
       circleout_pipe[0] <= 1;
     end
+
+    if(balllox == 0 | balllox == 7) begin
+      if(ballloy == 0 | ballloy == 1 | ballloy == 6 | ballloy == 7 ) begin
+        ballout_pipe[0] <= 0;
+      end else begin
+        ballout_pipe[0] <= 1;
+      end 
+    end else if(balllox == 1|balllox ==6) begin
+      if(ballloy == 0 | ballloy == 7) begin
+        ballout_pipe[0] <= 0;
+      end else begin
+        ballout_pipe[0] <= 1;
+      end 
+    end else begin
+      ballout_pipe[0] <= 1;
+    end
+
+
     imagesprite_pipe[0] <= isball;
     hcount_pipe[0] <= hcount_in;
     vcount_pipe[0] <= vcount_in;
 
-    angle30_pipe_h[0] <= ballx*8+3;
-    angle60_pipe_h[0] <= ballx*8+3;
-    angle90_pipe_h[0] <= ballx*8+3;
-    angle30_pipe_v[0] <= bally*8+4;
-    angle60_pipe_v[0] <= bally*8+4;
-    angle90_pipe_v[0] <= bally*8+4;
+    angle30_pipe_h[0] <= ballposx;
+    angle60_pipe_h[0] <= ballposx;
+    angle90_pipe_h[0] <= ballposx;
+    angle30_pipe_v[0] <= ballposy+1;
+    angle60_pipe_v[0] <= ballposy+1;
+    angle90_pipe_v[0] <= ballposy+1;
 
     angle30_pipe_h[1] <= angle30_pipe_h[0];
     angle60_pipe_h[1] <= angle60_pipe_h[0];
@@ -139,6 +163,7 @@ module map_sprite_1 #(
     end
     for (int j = 1;j<4;j=j+1) begin
       circleout_pipe[j] <= circleout_pipe[j-1];
+      ballout_pipe[j] <= ballout_pipe[j-1];
       vcount_pipe[j] <= vcount_pipe[j-1];
     end
   end
@@ -180,9 +205,9 @@ module map_sprite_1 #(
   );
 
   // Modify the module below to use your BRAMs!
-  assign red_out =    ((vcount_pipe[3] == angle30_pipe_v[3] & hcount_pipe[3] == angle30_pipe_h[3])|(vcount_pipe[3] == angle60_pipe_v[3] & hcount_pipe[3] == angle60_pipe_h[3])|(vcount_pipe[3] == angle90_pipe_v[3] & hcount_pipe[3] == angle90_pipe_h[3]))?8'b00000000:(imagesprite_pipe[3]==1?(circleout_pipe[3]==1?8'b11111111:finalcolors[23:16]):(finalcolors[23:16]==8'b00000000 ?(circleout_pipe[3] == 1?8'b00000000:8'b01111100):finalcolors[23:16]));
-  assign green_out =  ((vcount_pipe[3] == angle30_pipe_v[3] & hcount_pipe[3] == angle30_pipe_h[3])|(vcount_pipe[3] == angle60_pipe_v[3] & hcount_pipe[3] == angle60_pipe_h[3])|(vcount_pipe[3] == angle90_pipe_v[3] & hcount_pipe[3] == angle90_pipe_h[3]))?8'b00000000:(imagesprite_pipe[3]==1?(circleout_pipe[3]==1?8'b11111111:finalcolors[15:8]):(finalcolors[15:8]==8'b00000000 ?(circleout_pipe[3] == 1?8'b00000000:8'b11111100):finalcolors[15:8]));
-  assign blue_out =   ((vcount_pipe[3] == angle30_pipe_v[3] & hcount_pipe[3] == angle30_pipe_h[3])|(vcount_pipe[3] == angle60_pipe_v[3] & hcount_pipe[3] == angle60_pipe_h[3])|(vcount_pipe[3] == angle90_pipe_v[3] & hcount_pipe[3] == angle90_pipe_h[3]))?8'b11111111:(imagesprite_pipe[3]==1?(circleout_pipe[3]==1?8'b11111111:finalcolors[7:0]):(finalcolors[7:0]==8'b00000000 ?(circleout_pipe[3] == 1?8'b00000000:8'b00000000):finalcolors[7:0]));
+  assign red_out =    ((vcount_pipe[3] == angle30_pipe_v[3] & hcount_pipe[3] == angle30_pipe_h[3])|(vcount_pipe[3] == angle60_pipe_v[3] & hcount_pipe[3] == angle60_pipe_h[3])|(vcount_pipe[3] == angle90_pipe_v[3] & hcount_pipe[3] == angle90_pipe_h[3]))?8'b00000000:(imagesprite_pipe[3]==1?(ballout_pipe[3]==1?8'b11111111:finalcolors[23:16]):(finalcolors[23:16]==8'b00000000 ?(circleout_pipe[3] == 1?8'b00000000:8'b01111100):finalcolors[23:16]));
+  assign green_out =  ((vcount_pipe[3] == angle30_pipe_v[3] & hcount_pipe[3] == angle30_pipe_h[3])|(vcount_pipe[3] == angle60_pipe_v[3] & hcount_pipe[3] == angle60_pipe_h[3])|(vcount_pipe[3] == angle90_pipe_v[3] & hcount_pipe[3] == angle90_pipe_h[3]))?8'b00000000:(imagesprite_pipe[3]==1?(ballout_pipe[3]==1?8'b11111111:finalcolors[15:8]):(finalcolors[15:8]==8'b00000000 ?(circleout_pipe[3] == 1?8'b00000000:8'b11111100):finalcolors[15:8]));
+  assign blue_out =   ((vcount_pipe[3] == angle30_pipe_v[3] & hcount_pipe[3] == angle30_pipe_h[3])|(vcount_pipe[3] == angle60_pipe_v[3] & hcount_pipe[3] == angle60_pipe_h[3])|(vcount_pipe[3] == angle90_pipe_v[3] & hcount_pipe[3] == angle90_pipe_h[3]))?8'b11111111:(imagesprite_pipe[3]==1?(ballout_pipe[3]==1?8'b11111111:finalcolors[7:0]):(finalcolors[7:0]==8'b00000000 ?(circleout_pipe[3] == 1?8'b00000000:8'b00000000):finalcolors[7:0]));
 
 endmodule
 
